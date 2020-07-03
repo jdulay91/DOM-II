16af22a85661faff6a5b77315debdcca228ff96f
Julliann <jullianndulay@gmail.com>
2020-07-02 23:26:41 -0700

    answered prevent event

--- js/index.js
+++ js/index.js
@@ -5,66 +5,66 @@ const header = document.querySelector('header');
 //change background color
 
 
-//mouseover
-header.addEventListener('mouseover',function(){
-    header.style.backgroundColor ="#A8DADC"
-})
+// //mouseover
+// header.addEventListener('mouseover',function(){
+//     header.style.backgroundColor ="#A8DADC"
+// })
 
-//keydown
+// //keydown
 
-document.addEventListener('keydown',function(){
-    header.style.backgroundColor = "#F0F3BD";
-})
+// document.addEventListener('keydown',function(){
+//     header.style.backgroundColor = "#F0F3BD";
+// })
 
-// wheel
+// // wheel
 
-document.addEventListener('wheel',function(){
-    header.style.backgroundColor = "#C6DABF"
-})
+// document.addEventListener('wheel',function(){
+//     header.style.backgroundColor = "#C6DABF"
+// })
 
-// for (let i=0 ; i<navTags.length; i++){
-//     navTags[i].preventDefault();
-// }
-// // drag
-// const draggableImage = document.querySelector('.container .intro img')
-// draggableImage.addEventListener('drag',function() {
-// }, false);
+// //click
 const navTags = document.querySelectorAll('.nav-link');
 
-for(let i=0 ; i<navTags.length; i++){
-    navTags[i].addEventListener('click',function(){
-         navTags.preventDefault();
-        header.style.backgroundColor='green';
-    })
-}
+// for(let i=0 ; i<navTags.length; i++){
+//     navTags[i].addEventListener('click',function(){      
+//         header.style.backgroundColor='green';
+//         navTags.preventDefault();
+//     })
+// }
 
-//dblclick
-for(let i=0 ; i<navTags.length; i++){
-    navTags[i].addEventListener('dblclick',function(){
+//prevent event
+// navTags[0].addEventListener('click',function(e){ 
+//     e.preventDefault();
+//     header.style.backgroundColor='green';    
+    
+// })
+// //dblclick
+// for(let i=0 ; i<navTags.length; i++){
+//     navTags[i].addEventListener('dblclick',function(){
        
-        header.style.backgroundColor='pink';
-    })
-}
+//         header.style.backgroundColor='pink';
+//     })
+// }
 
-//loading
-window.addEventListener('load', function(){
-    header.style.backgroundColor = "gray";
-});
+// //loading
+// window.addEventListener('load', function(){
+//     header.style.backgroundColor = "gray";
+// });
 
-//mousedown
-window.addEventListener('mousedown',function(){
-    header.style.backgroundColor = "blue";
-})
+// //mousedown
+// window.addEventListener('mousedown',function(){
+//     header.style.backgroundColor = "blue";
+// })
 
-//mouseup
-window.addEventListener('mouseup',function(){
-    header.style.backgroundColor = "red";
-})
-//scroll
-window.addEventListener('scroll',function(){
-    header.style.backgroundColor = "#FFE1A8";
-})
-//mouseleave
-header.addEventListener('mouseleave',function(){
-    header.style.backgroundColor = "#5BC0BE";
-})
+// //mouseup
+// window.addEventListener('mouseup',function(){
+//     header.style.backgroundColor = "red";
+// })
+// //scroll
+// window.addEventListener('scroll',function(){
+//     header.style.backgroundColor = "#FFE1A8";
+// })
+// //mouseleave
+// header.addEventListener('mouseleave',function(){
+//     header.style.backgroundColor = "#5BC0BE";
+// })
